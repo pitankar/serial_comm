efb6d5adab674204c1050431c966d1505015ee71
Piyush Itankar <pitankar@gmail.com>
2017-11-05 13:52:02 +0530

    Adds base source code file with directions.
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,10 @@
+/*
+ * TODO
+ * 
+ * 1. Add a comment header explaing what the code in file does. Similar to this: https://raw.githubusercontent.com/torvalds/linux/master/include/linux/serial.h
+ * 2. Add only the needed header files, any global variable that you used and the main function.
+ * 3. Be sure to check for error codes and failure conditions. In case of failure, print the appropriate message and return an error code (-1, -2 etc)
+ * 4. Comment and intend the code correctly. Optimize of readibility.
+ * 5. Remove this block of comment.
+ * 
+ */
---
+++ src/serial_comm.c
@@ -0,0 +1,13 @@
+/*
+ * TODO
+ * 
+ * 1. Add a comment header explaing what the code in file does. Similar to this: https://github.com/torvalds/linux/blob/master/drivers/tty/tty_port.c
+ * 2. Add only the needed header files. Write the function implementation only. Include any global variable that you may be using in this file and make it static. 
+ * 3. Be sure to check for error codes and failure conditions. In case of failure, print the appropriate message and return an error code (-1, -2 etc)
+ * 4. Comment and intend the code correctly. Optimize of readibility.
+ *    - add comments (explaining the API) for only the helper functions that are used in this file and mark them as static.
+ *    - Do not add API comments for the public functions (functions that will be used in main.c)
+ *    - you may add comments inside the function as needed. Keep them limited.
+ * 5. Remove this block of comment.
+ * 
+ */
